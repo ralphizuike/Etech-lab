2d7fc40976e771bc641fc4c1d07de10bfc5b4a8e
enkengaf32 <89027119+enkengaf32@users.noreply.github.com>
2022-02-05 09:49:50 -0500

    Update worker-nodes.tf

--- worker-nodes.tf
+++ worker-nodes.tf
@@ -1,4 +1,4 @@
-##
+#
 # EKS Worker Nodes Resources
 #  * IAM role allowing Kubernetes actions to access other AWS services
 #  * EKS Node Group to launch worker nodes
@@ -43,6 +43,7 @@ resource "aws_eks_node_group" "demo" {
   node_group_name = "demo"
   node_role_arn   = aws_iam_role.demo-node.arn
   subnet_ids      = aws_subnet.demo[*].id
+  instance_types  = ["t3.small"]
 
   scaling_config {
     desired_size = 1
