d6a189559f0df8173ebc941fea1957a861963afd
enkengaf32 <89027119+enkengaf32@users.noreply.github.com>
2022-02-05 13:24:33 -0500

    Delete ec2-instance.tf

--- ec2-instance.tf
+++
@@ -1,8 +0,0 @@
-resource "aws_instance" "testserver" {
-        ami = "ami-0fcda042dd8ae41c7"
-        instance_type = "t2.micro"
-        tags = {
-           Name = "testserver"
-           Description = "use for demo"
-         }
-}
