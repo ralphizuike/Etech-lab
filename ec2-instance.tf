fb8cd6a0e52e03d98cdd974fbf89821ef553ac55
enkengaf32 <89027119+enkengaf32@users.noreply.github.com>
2022-02-05 13:14:44 -0500

    Create ec2-instance.tf

---
+++ ec2-instance.tf
@@ -0,0 +1,7 @@
+resource "aws_instance" "testserver" {
+        ami = "ami-03f4121463e28151e"
+        instance_type = "t2.micro"
+        tags = {
+           Name = "testserver"
+           Description = "use for demo"
+         }
