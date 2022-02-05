8b8fc69c858a7dda7a618784c6f48b433b67fa4c
enkengaf32 <89027119+enkengaf32@users.noreply.github.com>
2022-02-05 10:08:09 -0500

    Add files via upload

---
+++ providers.tf
@@ -0,0 +1,22 @@
+terraform {
+  required_version = ">= 0.12"
+}
+
+provider "aws" {
+  region = var.aws_region
+  access_key = "AKIASR6MHZNGMIO6LRNP"
+  secret_key = "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"
+}
+
+data "aws_availability_zones" "available" {}
+
+# Not required: currently used in conjunction with using
+# icanhazip.com to determine local workstation external IP
+# to open EC2 Security Group access to the Kubernetes cluster.
+# See workstation-external-ip.tf for additional information.
+provider "http" {}
+
+
+#kubectl create deployment nginx --image=nginx
+#kubectl create service nodeport nginx --tcp=80:80
+#
